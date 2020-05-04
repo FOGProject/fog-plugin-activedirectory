486cd3e79877a0ea06a70603759540119881d45e
Tom Elliott <tommygunsster@gmail.com>
2020-05-04 17:21:52 -0500

    Minor fixes here and there to clean up things a little.

--- config/globals.js
+++ config/globals.js
@@ -1,5 +1,5 @@
 module.exports.globals = {
-  authentiationMechanisms: [
+  authenticationMechanisms: [
     'ActiveDirectory'
   ]
 };
--- config/passport/activedirectory.js
+++ config/passport/activedirectory.js
@@ -5,26 +5,29 @@ const passport = require('passport'),
 passport.serializeUser(async function(user, done) {
   done(null, user);
 });
-passport.deserializeUser(async function(id, done) {
+passport.deserializeUser(async function(user, done) {
   done(null, user);
 });
 passport.use(new ADStrategy(
   adOpts.adopts,
   async function(profile, ad, done) {
-    for (var i = 0;i < permissions.memberOf.length;i++) {
-      let permKeys = Object.keys(permissions.memberOf[i]);
+    for (var i = 0;i < adperms.memberOf.length;i++) {
+      memberOf = adperms.memberOf[i];
+      let permKeys = Object.keys(memberOf);
       for (var j = 0;j < permKeys.length;j++) {
-        await ad.isUserMemberOf(profile._json.dn, permKeys[i], function(err, isMember) {
-          let user = profile;
+        member = permKeys[j];
+        await ad.isUserMemberOf(profile._json.dn, member, function(err, isMember) {
+          let user = profile,
+            permissions = memberOf[member].permissions;
           if (err) return done(err);
-          if (!isMember) continue;
+          if (!isMember) return;
           user.permissions = {};
-          user.permissions = _.merge(user.permissions, permissions, permissions.memberOf[permKeys[i]].permissions);
+          user.permissions = _.merge(user.permissions, permissions);
           user.isADAuth = true;
           return done(null, user, {message: 'Login Successful'});
         });
       }
     }
-    return done(null, user, {message: 'User not found'});
+    return done(null, false, {message: 'User not found'});
   }
 ));
