c235e71b46570dabaa061deb664c60150eec2066
Tom Elliott <tommygunsster@gmail.com>
2021-01-14 07:29:40 -0600

    Tweak our ad strategy

--- config/passport/activedirectory.js
+++ config/passport/activedirectory.js
@@ -4,19 +4,16 @@ const passport = require('passport'),
   uuid = require('uuid'),
   adperms = require('../adperms');
 let adUser;
-passport.serializeUser(async (user, done) => {
+passport.serializeUser(async function(user, done) {
   done(null, user.id);
 });
-passport.deserializeUser(async (id, done) => {
-  await Aduser.findOne({id}).populateAll().exec(async (err, user) => {
-    if (err) return done(err);
-    done(null, user);
+passport.deserializeUser(async function(id, done) {
+  await Aduser.findOne({id}).populateAll().exec(function(err, user) {
+    done(err, user);
   });
 });
 let permKeys = Object.keys(adperms.memberOf);
-let setMainUser = (err, user, message, cb) => {
-  if (err) return cb(err, false, {message: err});
-  if (!user) return cb(null, false, {message: 'No user found'});
+let setMainUser = (user, message, cb) => {
   adUser = user;
   cb(err, adUser, message);
 };
@@ -27,25 +24,23 @@ permKeys.forEach(async (member) => {
     adOpts.adopts,
     async (profile, ad, done) => {
       await Role.find({or: [{name: roleNames},{id: roleNames}]}, async (err, roles) => {
-        if (err) return setMainUser(null, false, {message: err}, done);
+        if (err) return done(err);
         for (var i = 0;i < roles.length;i++) {
           roleIDs.push(roles[i].id);
         }
         await ad.isUserMemberOf(profile._json.dn, member, async (err, isMember) => {
-          if (err) return setMainUser(err, false, {message: err}, done);
+          if (err) return done(err);
           let aduser = {
             objectGuid: uuid.v4(profile._json.dn),
             displayName: profile._json.displayName,
             roles: roleIDs
           };
-          await Aduser.findOrCreate({objectGuid: aduser.objectGuid}, aduser, async (err, user) => {
-            if (err) return setMainUser(err, false, {message: err}, done);
-            if (user) user.isADAuth = true;
-            await Aduser.findOne({id: user.id}).populate('roles').exec(async (err, user) => {
-              if (err) return setMainUser(err, user, {message: err}, done);
-              setMainUser(null, user, {message: 'Login Successful'}, done);
-            });
-          });
+          await Aduser.findOrCreate({objectGuid: aduser.objectGuid}, aduser).then((user) => {
+            user.isADAuth = true;
+            await Aduser.findOne({id: user.id}).populate('roles').then((user) => {
+              return setMainUser(user, {message: 'Login Successful'});
+            }).catch(done);
+          }).catch(done);
         });
       });
     }
